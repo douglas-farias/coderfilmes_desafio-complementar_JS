e3aba74cac37ca018de2a834208fe4eceb754d10
douglas-farias <140763165+douglas-farias@users.noreply.github.com>
2023-10-03 19:29:52 -0300

    Add files via upload

---
+++ barbie2023.js
@@ -0,0 +1,5 @@
+let botaoGeneros = document.getElementById("botaoGeneros");
+
+botaoGeneros.onclick = () => {
+    alert("A seção de busca por gêneros está em construção.Nos desculpe pelo incoveniente.")
+};
